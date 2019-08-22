6e566d8a36dcf29190ebc62a4303c93a279f5bac
nickdoudou <460410786@qq.com>
2019-08-22 22:18:19 +0800

    完成头部底部拼接

---
+++ minsu/static/js/contact.js
@@ -0,0 +1,6 @@
+// 利用jquery的load()方法把header.html和footer.html加载进来    
+$(function() {
+     $("#header").load('header.html');
+     $("#footer").load('footer.html');
+//      $(".plat").load('caourser.html');
+            })
