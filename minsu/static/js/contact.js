7c5757d88d313738551a4a91fe24118e5b8e7088
nickdoudou <460410786@qq.com>
2019-08-23 21:55:31 +0800

    完成首页拼接

--- minsu/static/js/contact.js
+++
@@ -1,6 +0,0 @@
-// 利用jquery的load()方法把header.html和footer.html加载进来    
-$(function() {
-     $("#header").load('header.html');
-     $("#footer").load('footer.html');
-//      $(".plat").load('caourser.html');
-            })
